4c07ee2dcf7923dc9e403e894c076ee21430fc29
AbhayTr <53339132+AbhayTr@users.noreply.github.com>
2020-01-06 10:34:55 +0530

    Add files via upload

--- DbUpdateJobService.java
+++ DbUpdateJobService.java
@@ -2,6 +2,7 @@ package com.example.job;
 
 import android.app.job.JobParameters; 
 import android.app.job.JobService; 
+import java.util.*;
 import android.widget.*;
 import android.app.Activity;
 import android.app.*;
@@ -18,17 +19,39 @@ import android.preference.PreferenceManager;
 
 public class DbUpdateJobService extends JobService
 { 
-static String con = "";
-int f = 1;
-SharedPreferences pref;
 
-   @Override public boolean onStartJob(final JobParameters jobParameters) 
+private boolean isAppRunning() 
+{ 
+ActivityManager m = (ActivityManager) this.getSystemService( ACTIVITY_SERVICE ); 
+List<ActivityManager.RunningTaskInfo> runningTaskInfoList = m.getRunningTasks(10); 
+Iterator<ActivityManager.RunningTaskInfo> itr = runningTaskInfoList.iterator(); 
+int n=0; 
+while(itr.hasNext())
+{ 
+n++; 
+itr.next(); 
+} 
+if(n==1)
+{ 
+return false; 
+} 
+return true; 
+}
+
+   @Override 
+public boolean onStartJob(final JobParameters jobParameters) 
 {  
-pref = getApplicationContext().getSharedPreferences("MyPref", 0);
-Toast.makeText(this, pref.getString("user", null), Toast.LENGTH_LONG).show();
+if (isAppRunning())
+{
+Toast.makeText(this, "Running", Toast.LENGTH_LONG).show();
+}
+else
+{
+Toast.makeText(this, "Not Running", Toast.LENGTH_LONG).show();
+}
 JobScheduler jobScheduler = (JobScheduler)getApplicationContext().getSystemService(JOB_SCHEDULER_SERVICE); 
 ComponentName componentName = new ComponentName(DbUpdateJobService.this, DbUpdateJobService.class.getName()); 
-JobInfo jobInfo = new JobInfo.Builder(1, componentName).setMinimumLatency(1000).build(); 
+JobInfo jobInfo = new JobInfo.Builder(1, componentName).setMinimumLatency(10000).build(); 
 jobScheduler.schedule(jobInfo);
 jobFinished(jobParameters, false);
 return true;
